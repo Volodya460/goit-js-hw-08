68c2f4ede44fe368f532a85a35c7ddbc2916c328
Volodya Kushch <volodyakushch@gmail.com>
2023-01-10 15:31:40 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,53 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
-console.log(`hello`);
+
+const gallaryBox = document.querySelector('.gallery');
+const gallery = creatGallary(galleryItems);
+// console.log(creatGallary(galleryItems));
+
+gallaryBox.innerHTML = gallery;
+
+function creatGallary(event) {
+  return event
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href='${original}'>
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+gallaryBox.addEventListener('click', onGallaryBoxClick);
+
+function onGallaryBoxClick(event) {
+  // console.log(event.target);
+  const bigImage = event.target.dataset.source;
+  // console.log(bigImage);
+  event.preventDefault();
+  const clickObject = event.target.classList.contains('gallery__image');
+  if (!clickObject) {
+    return;
+  }
+  const instance = simplelightbox.create(`
+	 <img src="${bigImage}" width="800" height="600">
+`);
+  instance.show();
+
+  gallaryBox.addEventListener('keydown', onEscapeClick);
+
+  function onEscapeClick(event) {
+    if (event.code === 'Escape') instance.close();
+  }
+}

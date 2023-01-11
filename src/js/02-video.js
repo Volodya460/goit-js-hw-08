8a41ac939d23639705a96f7ac858c55b7be2d2cb
Volodya Kushch <volodyakushch@gmail.com>
2023-01-11 16:56:33 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TOTALTIMEPLAY = 'videoplayer-current-time';
+const realTimePlayer = localStorage.getItem(TOTALTIMEPLAY);
+
+player.on(`timeupdate`, throttle(timesave, 1000));
+
+function timesave({ seconds }) {
+  localStorage.setItem(TOTALTIMEPLAY, seconds);
+}
+
+player.setCurrentTime(realTimePlayer);
